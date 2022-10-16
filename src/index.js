5147c81546050798a32e81aa6870e0a36110fd35
Kirill <bloodwingest@gmail.com>
2022-10-17 02:14:08 +0300

    feat: solved mac-address

--- src/index.js
+++ src/index.js
@@ -17,34 +17,16 @@
 
 let result = getDNSStats(['code.yandex.ru', 'music.yandex.ru', 'yandex.ru'])
 console.log(result); */
-function sortByHeight(arr) {
-    let indexes = [];
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i] === -1){
-            indexes.push(i)
-        }
-    }
 
-    
-    arr.sort(function (a, b) {
-        return a - b
-    })
+function isMAC48Address(str) {
+    let pattern = /\b[A-F0-9]+-[A-F0-9]+-[A-F0-9]+-[A-F0-9]+-[A-F0-9]+-[A-F0-9]+/g;
+    return pattern.test(str)
+  }
 
-    do{
-        for (let i = 0; i < arr.length; i++) {
-            if(arr[i] === -1){
-                arr.splice(i, 1)
-            }
-        }
-    } while (arr.includes(-1))
+let result = isMAC48Address('A1-1B-63-84-45-E6')
 
-    for (let i = 0; i < indexes.length; i++) {
-        arr.splice(indexes[i], 0, -1)
-    }
+//'00-1B-63-84-45-E6'
+//'Z1-1B-63-84-45-E6'
 
-    return arr;
-}
-
-let result = sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180])
 
 console.log(result);
--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function isMAC48Address(str) {
+  let pattern = /\b[A-F0-9]+-[A-F0-9]+-[A-F0-9]+-[A-F0-9]+-[A-F0-9]+-[A-F0-9]+/g;
+  return pattern.test(str)
 }
 module.exports = {
   isMAC48Address
