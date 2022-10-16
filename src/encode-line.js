5379679f8d9f467d6f0a9d414f6c3b0d87483b36
Kirill <bloodwingest@gmail.com>
2022-10-16 22:58:27 +0300

    feat:solved encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function encodeLine(str) {
+  let currentLetter;
+  let stringArray = [];
+  let count = 0;
+  for (let i = 0; i <= str.length; i++) {
+    if(i !== 0){
+      if(str[i] === str[i-1]){
+          count++;
+        }
+        if(str[i] !== str[i-1]){
+          if(count > 1){
+            stringArray.push(count);
+          }
+          stringArray.push(str[i-1]);
+          count = 0;
+          count++
+        }
+    } else {
+      currentLetter = str[i];
+      count++;
+    }
+  }
+  return stringArray.join('')
 }
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-function createDreamTeam(members) {
-    let letters = []
-    members.forEach(element => {
-      if(typeof element === 'string'){
-        letters.push(element.slice(0,1).toUpperCase())
-        console.log(element);
-        console.log(letters);
+function encodeLine(str) {
+    let currentLetter;
+    let stringArray = [];
+    let count = 0;
+    for (let i = 0; i <= str.length; i++) {
+      if(i !== 0){
+        if(str[i] === str[i-1]){
+            count++;
+          }
+          if(str[i] !== str[i-1]){
+            if(count > 1){
+              stringArray.push(count);
+            }
+            stringArray.push(str[i-1]);
+            count = 0;
+            count++
+          }
+      } else {
+        currentLetter = str[i];
+        count++;
       }
-    });
-    return letters.sort(function(a,b){
-      if (a < b) {
-        return -1;
-      }
-      if (a > b) {
-        return 1;
-      }
-      return 0;
-    }).join('')
+    }
+    return stringArray.join('')
   }
 
-let result = createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max'])
+let result = encodeLine('aaaatttt')
 console.log(result);
