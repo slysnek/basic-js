85cff6bb8a066ae08116bc8b24153a56da9ab9d3
Kirill <bloodwingest@gmail.com>
2022-10-17 22:34:54 +0300

    partially implemented transform-array

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
-const chainMaker = {
-  chain: [],
-  getLength() {
-    return this.chain.length
-  },
-
-  addLink(value) {
-      this.chain.push(`( ${value} )`)
-      return this
-  },
-
-  removeLink(position) {
-    if(!(position<=0 || position>this.chain.length || Number.isInteger(position))){
-      let pos = position-1;
-      this.chain.splice(pos, 1)
-    } else {
-      this.chain = []
-      throw Error ("You can't remove incorrect link!")
-    }
-    return this
-  },
-  reverseChain() {
-    this.chain.reverse();
-    return this;
-  },
-  finishChain() {
-    return this.chain
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
   }
-};
 
-chainMaker.addLink(function () { }).addLink('2nd').addLink('3rd').removeLink(2).reverseChain().finishChain(), '( 3rd )~~( function () { } )');
+  let finalArray = [...arr]
 
-console.log(chainMaker);
+  for (let i = 0; i < finalArray.length; i++) {
+    if (finalArray[i] === '--discard-next') {
+      if(i === finalArray.length - 1){
+        continue
+      } else{
+        finalArray.splice(i, 2)
+      }
+    }
+    if (finalArray[i] === '--discard-prev') {
+      if (i === 0) {
+        continue
+      } else {
+        finalArray.splice(i - 1, 1)
+      }
+    }
+    if (finalArray[i] === '--double-next') {
+      let temp = finalArray[i + 1];
+      finalArray.splice(i, 1, temp)
+    }
+    if (finalArray[i] === '--double-prev') {
+      let temp2 = finalArray[i - 1];
+      finalArray.splice(i, 1, temp2)
+    }
+  }
+  return finalArray.filter((el) => el !== '--discard-next' && el !== '--discard-prev')
+}
+
+let result = transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5])
+console.log(result);
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  if(!Array.isArray(arr)) {
+    throw new Error ("'arr' parameter must be an instance of the Array!")
+  }
+
+  let finalArray = [...arr]
+
+  for (let i = 0; i < finalArray.length; i++) {
+      if (finalArray[i] === '--discard-next') {
+        if(i === finalArray.length - 1){
+          continue
+        } else{
+          finalArray.splice(i, 2)
+        }
+      }
+      if (finalArray[i] === '--discard-prev') {
+        if(i===0){
+          continue
+        } else{
+          finalArray.splice(i-1, 2)
+        }
+      }
+      if (finalArray[i] === '--double-next') {
+/*         let temp = finalArray[i+1];
+        finalArray.splice(i, 1, temp) */
+        finalArray[i] = finalArray[i+1]
+      }
+      if (finalArray[i] === '--double-prev') {
+/*         let temp2 = finalArray[i-1];
+        finalArray.splice(i, 1, temp2) */
+        finalArray[i] = finalArray[i-1]
+      }
+  }
+  return finalArray.filter((el) => el !== '--discard-next' && el !== '--discard-prev' && el !== undefined)
 }
 
 module.exports = {
