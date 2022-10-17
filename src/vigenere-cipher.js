69fe9bfe63ee5fea837b09dd51adcf66df1ff699
Kirill <bloodwingest@gmail.com>
2022-10-18 01:40:32 +0300

    feat: solved vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,92 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction) {
+    this.direction = direction;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    this.upperBorderZ = 90
+    this.lowerBorderA = 65
+    this.numberOfLetters = this.alphabet.length;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw Error("Incorrect arguments!");
+    }
+
+    let upperMessage = message.toUpperCase();
+    let upperKey = key.toUpperCase();
+    let cypher = '';
+    let count = 0;
+  
+    while (upperKey.length < upperMessage.length) {
+      upperKey += upperKey;
+    }
+
+    for (let i = 0; i < upperMessage.length; i++) {
+      if (this.alphabet.includes(upperMessage[i])) {
+        let letterWithCypher = upperMessage.charCodeAt(i) + upperKey.charCodeAt(count) - this.lowerBorderA;
+        console.log(letterWithCypher);
+        if (letterWithCypher > this.upperBorderZ) {
+          letterWithCypher = letterWithCypher - this.numberOfLetters;
+        }
+        cypher = cypher + String.fromCharCode(letterWithCypher);
+        count++;
+      }
+  
+      else {
+        cypher = cypher + upperMessage[i];
+      }
+    }
+    
+    if (this.direction === true ||
+       this.direction === undefined) {
+      return cypher;
+    }
+
+    let cryptedMessage = cypher.split('').reverse().join('');
+    return cryptedMessage;
+  }
+    
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw Error("Incorrect arguments!");
+    }
+
+    let upperMessage = encryptedMessage.toUpperCase();
+    let upperKey = key.toUpperCase();
+  
+    while (upperKey.length < upperMessage.length) {
+      upperKey += upperKey;
+    }
+
+    let cypher = '';
+    let count = 0;
+
+    for (let i = 0; i < upperMessage.length; i++) {
+      if (this.alphabet.includes(upperMessage[i])) {
+        let letterWithoutCipher = upperMessage.charCodeAt(i) - upperKey.charCodeAt(count) + this.lowerBorderA;
+        console.log(letterWithoutCipher);
+        if (letterWithoutCipher < this.lowerBorderA) {
+          letterWithoutCipher = letterWithoutCipher + this.numberOfLetters;
+        }
+        cypher = cypher + String.fromCharCode(letterWithoutCipher);
+        count++;
+      }
+        else {
+        cypher = cypher + upperMessage[i];
+      }
+    }
+        
+    if (this.direction === true ||
+       this.direction === undefined) {
+      return cypher;
+    }
+     
+    let decryptedMessage = cypher.split('').reverse().join('');
+    return decryptedMessage;
   }
 }
 
